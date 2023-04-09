578962b88414f179b920e1be4ced105bb9867547
shreshthgupta7 <shreshthgupta7@gmail.com>
2023-04-09 12:15:38 +0530

    Hi

--- src/Pen/GelPen.java
+++ src/Pen/GelPen.java
@@ -4,7 +4,7 @@ public class GelPen extends Pen{
     refil r;
     GelPen(refil r)
     {
-
+        this.r=r;
     }
     public void write(){
     writebehaviour wb=writebehaviourfactory.style("addgel");
